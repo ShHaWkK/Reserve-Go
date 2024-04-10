c2b2a8053a73abacff7a178210cf0a4f2d28429b
ShHaWkK <alexandreuzan9@gmail.com>
2024-04-11 00:16:08 +0200

    Update main ADD Retrieve Reservations by Room and Date

--- main.go
+++ main.go
@@ -76,12 +76,16 @@ func main() {
 		case "6":
 			viewReservations(db, scanner)
 		case "7":
-			showHelp()
+			viewReservationsByRoom(db, scanner)
 		case "8":
+			viewReservationsByDate(db, scanner)
+		case "9":
+			showHelp()
+		case "10":
 			fmt.Println("Merci d'avoir utilisé le service. À bientôt !")
 			return
 		default:
-			fmt.Println("Option non valide. Veuillez choisir une option entre 1 et 7.") // Mise à jour de ce message pour inclure toutes les options
+			fmt.Println("Option non valide. Veuillez choisir une option entre 1 et 10.")
 		}
 	}
 }
@@ -130,9 +134,10 @@ func showMenu() {
 	fmt.Println("4. Créer une réservation")
 	fmt.Println("5. Annuler une réservation")
 	fmt.Println("6. Visualiser les réservations")
-
-	fmt.Println("7. Aide")
-	fmt.Println("8. Quitter")
+	fmt.Println("7. Récupérer les réservations par salle")
+	fmt.Println("8. Récupérer les réservations par date")
+	fmt.Println("9. Aide")
+	fmt.Println("10. Quitter")
 	fmt.Print("\nChoisissez une option : ")
 }
 
@@ -335,6 +340,90 @@ func createReservation(db *sql.DB, scanner *bufio.Scanner) {
 	navigationOptions(db, scanner)
 }
 
+/*
+*	Récupérer les réservations par salle
+ */
+func viewReservationsByRoom(db *sql.DB, scanner *bufio.Scanner) {
+	fmt.Print("Entrez l'ID de la salle (nombre entier) : ")
+	scanner.Scan()
+	roomIDStr := scanner.Text()
+
+	// Conversion de l'ID de la salle en int
+	roomID, err := strconv.Atoi(roomIDStr)
+	if err != nil {
+		fmt.Println("Erreur : ID de salle invalide. Veuillez entrer un nombre.")
+		return
+	}
+
+	// Vérifier si la salle existe
+	if !isRoomExists(db, roomID) {
+		fmt.Println("La salle avec l'ID", roomID, "n'existe pas.")
+		return
+	}
+
+	// Appel à getReservationsByRoom avec l'ID de la salle
+	reservations, err := getReservationsByRoom(db, roomID)
+	if err != nil {
+		fmt.Println("Erreur lors de la récupération des réservations :", err)
+		return
+	}
+
+	// Affichage des réservations
+	if len(reservations) == 0 {
+		fmt.Println("Aucune réservation trouvée pour la salle", roomID)
+		return
+	}
+
+	fmt.Println("Réservations pour la salle", roomID)
+	for _, reservation := range reservations {
+		fmt.Printf("ID: %d, Date: %s, Début: %s, Fin: %s\n", reservation.ID, reservation.Date, reservation.StartTime, reservation.EndTime)
+	}
+}
+
+/*
+*	Récupérer les réservations par salle
+ */
+func getReservationsByRoom(db *sql.DB, roomID int) ([]Reservation, error) {
+	var reservations []Reservation
+	query := "SELECT id, room_id, date, start_time, end_time FROM reservations WHERE room_id = ?"
+	rows, err := db.Query(query, roomID)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var r Reservation
+		if err := rows.Scan(&r.ID, &r.RoomID, &r.Date, &r.StartTime, &r.EndTime); err != nil {
+			return nil, err
+		}
+		reservations = append(reservations, r)
+	}
+	return reservations, nil
+}
+
+/*
+*	Récupérer les réservations par date
+ */
+func getReservationsByDate(db *sql.DB, date string) ([]Reservation, error) {
+	var reservations []Reservation
+	query := "SELECT id, room_id, date, start_time, end_time FROM reservations WHERE date = ?"
+	rows, err := db.Query(query, date)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var r Reservation
+		if err := rows.Scan(&r.ID, &r.RoomID, &r.Date, &r.StartTime, &r.EndTime); err != nil {
+			return nil, err
+		}
+		reservations = append(reservations, r)
+	}
+	return reservations, nil
+}
+
 func isRoomAvailable(db *sql.DB, roomID, date, startTime, endTime string) bool {
 	query := `SELECT COUNT(*) FROM reservations 
               WHERE room_id = ? 
@@ -350,8 +439,7 @@ func isRoomAvailable(db *sql.DB, roomID, date, startTime, endTime string) bool {
 
 	return count == 0
 }
-
-func isRoomExists(db *sql.DB, roomID string) bool {
+func isRoomExists(db *sql.DB, roomID int) bool {
 	var exists bool
 	err := db.QueryRow("SELECT EXISTS(SELECT 1 FROM rooms WHERE id = ?)", roomID).Scan(&exists)
 	return err == nil && exists
@@ -423,6 +511,41 @@ func viewReservations(db *sql.DB, scanner *bufio.Scanner) {
 
 	// Offre des options de navigation après avoir visualisé les réservations.
 	navigationOptions(db, scanner)
+}
+
+/*
+*	Récupérer et afficher les réservations par date
+ */
+
+// Fonction pour récupérer et afficher les réservations par date
+func viewReservationsByDate(db *sql.DB, scanner *bufio.Scanner) {
+	fmt.Print("Entrez la date pour laquelle vous souhaitez voir les réservations (format YYYY-MM-DD) : ")
+	scanner.Scan()
+	date := scanner.Text()
+
+	// Validation de la date (simple vérification de format)
+	if _, err := time.Parse("2006-01-02", date); err != nil {
+		fmt.Println("Erreur : Format de date invalide.")
+		return
+	}
+
+	// Appel à la fonction pour obtenir les réservations
+	reservations, err := getReservationsByDate(db, date)
+	if err != nil {
+		fmt.Println("Erreur lors de la récupération des réservations :", err)
+		return
+	}
+
+	// Affichage des réservations
+	if len(reservations) == 0 {
+		fmt.Println("Aucune réservation trouvée pour la date", date)
+		return
+	}
+
+	fmt.Println("Réservations pour la date", date)
+	for _, reservation := range reservations {
+		fmt.Printf("ID Réservation: %d, ID Salle: %d, Début: %s, Fin: %s\n", reservation.ID, reservation.RoomID, reservation.StartTime, reservation.EndTime)
+	}
 }
 
 /*
